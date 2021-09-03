cbaf0896452d39f4b6a3a2fbc46f3db625fbbc12
LineWelder <redalerm@gmail.com>
2021-09-03 18:11:07 +0300

    Initial commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mycraft")]
+[assembly: AssemblyCompany("LineWelder")]
+[assembly: AssemblyProduct("Mycraft")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("a2fc1a23-6b9e-42bb-bc37-787cc25e3614")]
---
+++ GameWindow.cs
@@ -0,0 +1,61 @@
+﻿using OpenGL;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Mycraft
+{
+    public class GameWindow : Form
+    {
+        private readonly GlControl glControl;
+
+        public GameWindow()
+        {
+            SuspendLayout();
+
+            Name = "GameWindow";
+            Text = "Mycraft";
+            ClientSize = new Size(1920, 1080);
+
+            glControl = new GlControl
+            {
+                Name = "GLControl",
+                Dock = DockStyle.Fill,
+
+                ColorBits = 24u,
+                DepthBits = 0u,
+                MultisampleBits = 0u,
+                StencilBits = 0u
+            };
+
+            Resize += OnResized;
+            glControl.ContextCreated += OnContextCreated;
+            glControl.ContextDestroying += OnContextDestroyed;
+            glControl.Render += Render;
+
+            Controls.Add(glControl);
+            ResumeLayout(false);
+        }
+
+        private void OnResized(object sender, EventArgs e)
+        {
+            Gl.Viewport(0, 0, ClientSize.Width, ClientSize.Height);
+        }
+
+        private void OnContextCreated(object sender, GlControlEventArgs e)
+        {
+            Gl.Viewport(0, 0, ClientSize.Width, ClientSize.Height);
+            Console.WriteLine(((GlControl)sender).Size);
+        }
+
+        private void Render(object sender, GlControlEventArgs e)
+        {
+            Gl.Clear(ClearBufferMask.ColorBufferBit);
+        }
+
+        private void OnContextDestroyed(object sender, GlControlEventArgs e)
+        {
+            
+        }
+    }
+}
