e9406143bf87c87f648293554fd07abdf00767dd
LineWelder <redalerm@gmail.com>
2021-09-10 19:26:03 +0300

    Refactored all the shader related code

--- GameWindow.cs
+++ GameWindow.cs
@@ -11,7 +11,8 @@ namespace Mycraft
     {
         private readonly GlControl glControl;
 
-        private ShaderProgram coloredProgram, program;
+        private AttributeColoredShader attributeColoredShader;
+        private OneColoredShader oneColoredShader;
         private VertexArray triangle, origin;
 
         private readonly float[] triangleVertices =
@@ -31,54 +32,9 @@ namespace Mycraft
             0f, 0f, 1f, 0f, 0f, 1f
         };
 
-        private readonly string coloredVertexSource =
-@"#version 330 core
-
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 color;
-uniform mat4 mvp;
-out vec3 vtxColor;
-
-void main()
-{
-    vtxColor = color;
-    gl_Position = mvp * vec4(position, 1.0);
-}";
-
-        private readonly string coloredFragmentSource =
-@"#version 330 core
-in vec3 vtxColor;
-
-void main()
-{
-    gl_FragColor = vec4(vtxColor, 1.0);
-}";
-
-        private readonly string vertexSource =
-@"#version 330 core
-
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 color;
-uniform mat4 mvp;
-
-void main()
-{
-    gl_Position = mvp * vec4(position, 1.0);
-}";
-
-        private readonly string fragmentSource =
-@"#version 330 core
-uniform vec3 color;
-
-void main()
-{
-    gl_FragColor = vec4(color, 1.0);
-}";
-
-        private Matrix4x4f projection;
-
         private const float MOVEMENT_SPEED = .05f, ROTATION_SPEED = .03f;
         private readonly Camera camera;
+        private Matrix4x4f projection;
 
         public GameWindow()
         {
@@ -118,7 +74,6 @@ void main()
         private void OnResized(object sender, EventArgs e)
         {
             Gl.Viewport(0, 0, ClientSize.Width, ClientSize.Height);
-
             projection = Matrix4x4f.Perspective(70, (float)ClientSize.Width / ClientSize.Height, .01f, 100f);
         }
 
@@ -126,8 +81,8 @@ void main()
         {
             OnResized(null, null);
 
-            coloredProgram = new ShaderProgram(coloredVertexSource, coloredFragmentSource);
-            program = new ShaderProgram(vertexSource, fragmentSource);
+            attributeColoredShader = new AttributeColoredShader();
+            oneColoredShader = new OneColoredShader();
 
             triangle = new VertexArray(PrimitiveType.Triangles, triangleVertices, new int[] { 3 });
             origin = new VertexArray(PrimitiveType.Lines, originVertices, new int[] { 3, 3 });
@@ -135,10 +90,8 @@ void main()
             Gl.ClearColor(0.53f, 0.81f, 0.98f, 1f);
             Gl.Enable(EnableCap.DepthTest);
 
-            Gl.UseProgram(program.glId);
-            int colorLocation = Gl.GetUniformLocation(program.glId, "color");
-            if (colorLocation < 0) throw new InvalidOperationException("color not found");
-            Gl.Uniform3f(colorLocation, 1, new Vertex3f(0.98f, 0.86f, 0.87f));
+            Gl.UseProgram(oneColoredShader.glId);
+            Gl.Uniform3f(oneColoredShader.colorLocation, 1, new Vertex3f(0.98f, 0.86f, 0.87f));
         }
 
         private void OnContextUpdate(object sender, GlControlEventArgs e)
@@ -160,18 +113,19 @@ void main()
             Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             Matrix4x4f mvp = projection * camera.TransformMatrix;
 
-            Gl.UseProgram(program.glId);
-            program.MVP = mvp;
+            Gl.UseProgram(oneColoredShader.glId);
+            Gl.UniformMatrix4f(oneColoredShader.mvpLocation, 1, false, mvp);
             triangle.Draw();
 
-            Gl.UseProgram(coloredProgram.glId);
-            coloredProgram.MVP = mvp;
+            Gl.UseProgram(attributeColoredShader.glId);
+            Gl.UniformMatrix4f(attributeColoredShader.mvpLocation, 1, false, mvp);
             origin.Draw();
         }
 
         private void OnContextDestroyed(object sender, GlControlEventArgs e)
         {
-            program.Dispose();
+            oneColoredShader.Dispose();
+            attributeColoredShader.Dispose();
             triangle.Dispose();
         }
     }
---
+++ Graphics/AttributeColoredShader.cs
@@ -0,0 +1,41 @@
+﻿using OpenGL;
+using System;
+
+namespace Mycraft.Graphics
+{
+    public class AttributeColoredShader : ShaderProgram
+    {
+        private static readonly string VERTEX_SOURCE =
+@"#version 330 core
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 color;
+uniform mat4 mvp;
+out vec3 vtxColor;
+
+void main()
+{
+    vtxColor = color;
+    gl_Position = mvp * vec4(position, 1.0);
+}";
+
+        private const string FRAGMENT_SOURCE =
+@"#version 330 core
+in vec3 vtxColor;
+
+void main()
+{
+    gl_FragColor = vec4(vtxColor, 1.0);
+}";
+
+        public readonly int mvpLocation; 
+
+        public AttributeColoredShader()
+            : base(VERTEX_SOURCE, FRAGMENT_SOURCE)
+        {
+            mvpLocation = Gl.GetUniformLocation(glId, "mvp");
+            if (mvpLocation < 0)
+                throw new InvalidOperationException("MVP variable not found");
+        }
+    }
+}
