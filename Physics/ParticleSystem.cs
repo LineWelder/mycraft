8839dca76404e8dc9615126f8480b918ab8b5468
LineWelder <redalerm@gmail.com>
2021-09-16 21:50:26 +0300

    Added gravity to the particles

--- Physics/ParticleSystem.cs
+++ Physics/ParticleSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Mycraft.Blocks;
 using Mycraft.Graphics;
+using Mycraft.World;
 using OpenGL;
 using System;
 using System.Collections.Generic;
@@ -8,47 +9,69 @@ namespace Mycraft.Physics
 {
     public class ParticleSystem : VertexArray
     {
-        private struct Particle
+        private class Particle : FallingBox
         {
-            public Vertex3f position;
-            public Vertex3f velocity;
+            public Block block;
+
+            public Particle(GameWorld world, Block block, Vertex3f position, Vertex3f velocity)
+                : base(world, position, new Vertex3f())
+            {
+                this.block = block;
+                Velocity = velocity;
+            }
         }
 
-        private readonly Particle[] particles;
+        private readonly List<Particle> particles;
+        private readonly GameWorld world;
         private readonly float size;
-        private readonly Block block;
 
         public float GetRangedRandom(Random rand, float start, float end)
             => (float)(rand.NextDouble() * (end - start) + start);
 
-        public ParticleSystem(Vertex3f spawnAreaStart, Vertex3f spawnAreaEnd, int count, float size, Block block)
+        public ParticleSystem(GameWorld world, float size)
             : base(PrimitiveType.Quads, new int[] { 3, 2, 2 })
         {
-            particles = new Particle[count];
+            particles = new List<Particle>();
+            this.world = world;
             this.size = size;
-            this.block = block;
+            UpdateVertices();
+        }
+
+        public void Spawn(Vertex3f spawnAreaStart, Vertex3f spawnAreaEnd, int count, Block block)
+        {
+            Particle[] newParticles = new Particle[count];
 
             Random rand = new Random();
             for (int i = 0; i < count; i++)
-                particles[i] = new Particle
-                {
-                    position = new Vertex3f(
+                newParticles[i] = new Particle(
+                    world, block,
+                    new Vertex3f(
                         GetRangedRandom(rand, spawnAreaStart.x, spawnAreaEnd.x),
                         GetRangedRandom(rand, spawnAreaStart.y, spawnAreaEnd.y),
                         GetRangedRandom(rand, spawnAreaStart.z, spawnAreaEnd.z)
                     ),
-                    velocity = new Vertex3f()
-                };
+                    new Vertex3f()
+                );
+
+            particles.AddRange(newParticles);
+            UpdateVertices();
         }
 
-        public void UpdateVertices()
+        public void Update(double deltaTime)
+        {
+            foreach (Particle particle in particles)
+                particle.Update(deltaTime);
+            UpdateVertices();
+        }
+          
+        private void UpdateVertices()
         {
             List<float> mesh = new List<float>();
 
             foreach (Particle particle in particles)
             {
-                Vertex4f texCoords = Block.GetTextureCoords(block.GetTexture(BlockSide.Top));
-                Vertex3f pos = particle.position;
+                Vertex4f texCoords = Block.GetTextureCoords(particle.block.GetTexture(BlockSide.Top));
+                Vertex3f pos = particle.Position;
                 float offset = size / 2f;
 
                 mesh.AddRange(new float[] {
