15de2cfb1d38236919f4f32fa655794274866398
LineWelder <redalerm@gmail.com>
2021-10-02 23:19:55 +0200

    Separated the game logic from the window class

--- GameWindow.cs
+++ GameWindow.cs
@@ -21,21 +21,12 @@ namespace Mycraft
 {
     public class GameWindow : Form
     {
-        private const float MOVEMENT_ACCELERATION = 20f, MOVEMENT_SPEED = 3.7f;
         private const float MOUSE_SENSIVITY = .003f;
 
         private readonly GlControl glControl;
         private readonly Stopwatch stopwatch;
-        private Matrix4x4f projection;
 
-        private Origin origin;
-        private GameWorld world;
-        private GUIRectangle cross;
-        private ParticleSystem particles;
-
-        private SmoothChangingVertex2f playerMovement;
-        private Player player;
-        private Hotbar hotbar;
+        private readonly Game game;
 
         public GameWindow()
         {
@@ -75,15 +66,13 @@ namespace Mycraft
 
             stopwatch = new Stopwatch();
             stopwatch.Start();
+
+            game = new Game();
         }
 
         private void OnMouseWheel(object sender, MouseEventArgs e)
         {
-            int select = hotbar.Selected - e.Delta / 120;
-            if (select < 0)
-                hotbar.Selected = Hotbar.CAPACITY - 1;
-            else
-                hotbar.Selected = select % Hotbar.CAPACITY;
+            game.MoveHotbarSelection(-e.Delta / 120);
         }
 
         private (float dx, float dy) GrabCursor()
@@ -105,60 +94,20 @@ namespace Mycraft
         private void OnMouseMove(object sender, MouseEventArgs e)
         {
             var (dx, dy) = GrabCursor();
-            player.RotateCamera(MOUSE_SENSIVITY * dx, -MOUSE_SENSIVITY * dy);
+            game.RotateCamera(MOUSE_SENSIVITY * dx, -MOUSE_SENSIVITY * dy);
         }
 
         private void OnMouseDown(object sender, MouseEventArgs e)
         {
-            if (player.Selection.IsSelected)
+            switch (e.Button)
             {
-                if (e.Button == MouseButtons.Left)
-                {
-                    Vertex3i position = player.Selection.Position;
-                    Block block = world.GetBlock(
-                        position.x,
-                        position.y,
-                        position.z
-                    );
+                case MouseButtons.Left:
+                    game.BreakBlock();
+                    break;
 
-                    world.SetBlock(
-                        position.x,
-                        position.y,
-                        position.z,
-                        BlockRegistry.Air
-                    );
-
-                    particles.Spawn(
-                        (Vertex3f)position,
-                        (Vertex3f)position + new Vertex3f(1f, 1f, 1f),
-                        20, block
-                    );
-                }
-                else if (e.Button == MouseButtons.Right)
-                {
-                    if (!(hotbar.SelectedBlock is null))
-                    {
-                        Vertex3i placeBlockCoords = Block.GetNeighbour(
-                            player.Selection.Position,
-                            player.Selection.Side
-                        );
-                        
-                        if (!hotbar.SelectedBlock.HasCollider ||
-                            !player.Intersects(
-                                new AABB(
-                                    (Vertex3f)placeBlockCoords,
-                                    new Vertex3f(1f, 1f, 1f)
-                                )
-                            )
-                        )
-                            world.SetBlock(
-                                placeBlockCoords.x,
-                                placeBlockCoords.y,
-                                placeBlockCoords.z,
-                                hotbar.SelectedBlock
-                            );
-                    }
-                }
+                case MouseButtons.Right:
+                    game.PlaceBlock();
+                    break;
             }
         }
            
@@ -166,79 +115,18 @@ namespace Mycraft
         {
             if (!Resources.AreLoaded) return;
 
-            // Set the viewport and the projection
-
             Gl.Viewport(0, 0, ClientSize.Width, ClientSize.Height);
-            projection = Matrix4x4f.Perspective(
-                70, (float)ClientSize.Width / ClientSize.Height,
-                .01f, GameWorld.UNLOAD_DISTANCE * Chunk.SIZE * 2f 
-            );
-
-            Gl.UseProgram(Resources.GUIShader.glId);
-            Resources.GUIShader.Projection = Matrix4x4f.Ortho2D(0f, ClientSize.Width - 1, ClientSize.Height - 1, 0f);
-
-            // Update the GUI
-
-            int pixelSize = ClientSize.Height / 200;
-
-            cross = new GUIRectangle(
-                new Vertex2i(
-                    ClientSize.Width / 2 - 6 * pixelSize,
-                    ClientSize.Height / 2 - 6 * pixelSize
-                ),
-                new Vertex2i(12 * pixelSize, 12 * pixelSize)
-            );
-
-            hotbar = new Hotbar(
-                new Vertex2i(
-                    ClientSize.Width / 2 - 91 * pixelSize,
-                    ClientSize.Height - 21 * pixelSize
-                ),
-                pixelSize,
-                0, new Block[]
-                {
-                    BlockRegistry.Stone,
-                    BlockRegistry.Grass,
-                    BlockRegistry.Dirt,
-                    BlockRegistry.Log,
-                    BlockRegistry.Leaves,
-                    BlockRegistry.Water,
-                    null, null, null, null
-                }
-            );
+            game.Resize(ClientSize.Width, ClientSize.Height);
         }
 
         private void OnContextCreated(object sender, GlControlEventArgs e)
         {
-            Resources.LoadAll();
-
-            // Configure the graphics
-
-            Gl.LineWidth(2f);
-            Gl.Enable(EnableCap.Multisample);
-            Gl.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
-
-            Gl.UseProgram(Resources.GameWorldShader.glId);
-            Resources.GameWorldShader.Alpha = .6f;
-
-            // Screen stuff
-
-            OnResized(null, null);
             Cursor.Hide();
             GrabCursor();
 
-            // Create the game objects
-
-            origin = new Origin();
-
-            world = new GameWorld(new SimpleWorldGenerator());
-            world.GenerateSpawnArea();
-
-            playerMovement = new SmoothChangingVertex2f(new Vertex2f(), MOVEMENT_ACCELERATION);
-            player = new Player(world, new Vertex3f(.5f, world.GetGroundLevel(0, 0) + 1f, .5f));
-            world.Update(player.camera.Position, true);
-
-            particles = new ParticleSystem(world, .2f, .5d);
+            Resources.LoadAll();
+            game.Resize(ClientSize.Width, ClientSize.Height);
+            game.Init();
         }
 
         private void OnContextUpdate(object sender, GlControlEventArgs e)
@@ -247,143 +135,19 @@ namespace Mycraft
             stopwatch.Restart();
 
             Text = $"Mycraft - UPS: {(int)Math.Floor(1d / deltaTime)}";
-
-            // Player movement
-
-            int forwardInput    = FuncUtils.GetInput1d(Keys.W, Keys.S);
-            int horizontalInput = FuncUtils.GetInput1d(Keys.D, Keys.A);
-
-            if (forwardInput != 0 || horizontalInput != 0)
-                playerMovement.Value = new Vertex2f(
-                    forwardInput, horizontalInput
-                ).Normalized * MOVEMENT_SPEED;
-            else
-                playerMovement.Value = new Vertex2f();
-                
-            playerMovement.Update(deltaTime);
-            player.MoveRelativeToYaw(
-                (float)(playerMovement.Value.x * deltaTime),
-                (float)(playerMovement.Value.y * deltaTime)
-            );
-
-            Vertex3f velocity = player.Velocity;
-
-            if (FuncUtils.IsKeyPressed(Keys.Space))
-            {
-                if (player.IsGrounded)
-                    velocity.y = 6f;
-
-                if (player.IsInWater)
-                    velocity.y += (float)(20f * deltaTime);
-            }
-
-            // Jump off the void
-
-            if (player.Position.y < -64f && velocity.y < 0f)
-                velocity.y *= -1f;
-
-            player.Velocity = velocity;
-
-            // float speed = (float)(deltaTime * MOVEMENT_SPEED);
-            // camera.MoveRelativeToYaw(
-            //     speed * forwardInput,
-            //     speed * horizontalInput
-            // );
-            // camera.Translate(0f, FuncUtils.GetInput1d(Keys.Space, Keys.LShiftKey) * speed, 0f);
-
-            // Update the game objects
-
-            player.Update(deltaTime);
-            world.Update(player.camera.Position);
-            particles.Update(deltaTime);
-
-            // Update the graphics
-
-            if (player.camera.Position.y < 0)
-                Gl.ClearColor(.05f, .05f, .05f, 1f);
-            else
-                Gl.ClearColor(0.53f, 0.81f, 0.98f, 1f);
+            game.Update(deltaTime);
         }
 
         private void Render(object sender, GlControlEventArgs e)
         {
             Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            Matrix4x4f vp = projection * player.camera.TransformMatrix;
-
-            // Draw the world
-
-            Gl.UseProgram(Resources.GameWorldShader.glId);
-            Gl.Enable(EnableCap.DepthTest);
-
-            Resources.GameWorldShader.MVP = vp;
-            world.Draw();
-
-            // Draw particles
-
-            Gl.UseProgram(Resources.ParticleShader.glId);
-            Resources.ParticleShader.View = player.camera.TransformMatrix;
-            Resources.ParticleShader.Projection = projection;
-            Resources.BlocksTexture.Bind();
-            particles.Draw();
-
-            // Draw UI stuff
-
-            Gl.UseProgram(Resources.WorldUIShader.glId);
-            Resources.WorldUIShader.VP = vp;
-            player.Selection.Draw();
-
-            Resources.WorldUIShader.Model = Matrix4x4f.Identity;
-            origin.Draw();
-
-            // Draw vignette
-
-            Gl.Disable(EnableCap.DepthTest);
-            Gl.Disable(EnableCap.CullFace);
-
-            Block block = world.GetBlock(
-                (int)Math.Floor(player.camera.Position.x),
-                (int)Math.Floor(player.camera.Position.y),
-                (int)Math.Floor(player.camera.Position.z)
-            );
-
-            if (block is LiquidBlock)
-            {
-                Gl.UseProgram(Resources.OverlayShader.glId);
-
-                Gl.Enable(EnableCap.Blend);
-
-                Resources.BlocksTexture.Bind();
-                Vertex4f texture = Block.GetTextureCoords(block.GetTexture(BlockSide.Top));
-                using (VertexArray overlay = new VertexArray(
-                    PrimitiveType.Quads, new int[] { 2, 2 },
-                    new float[]
-                    {
-                         1f,  1f,  texture.z, texture.y,
-                         1f, -1f,  texture.z, texture.w,
-                        -1f, -1f,  texture.x, texture.w,
-                        -1f,  1f,  texture.x, texture.y
-                    }
-                )) overlay.Draw();
-            }
-
-            // Draw GUI
-
-            Gl.UseProgram(Resources.GUIShader.glId);
-            Gl.Disable(EnableCap.Blend);
-
-            Resources.CrossTexture.Bind();
-            cross.Draw();
-            
-            hotbar.Draw();
+            game.Draw();
         }
 
         private void OnContextDestroyed(object sender, GlControlEventArgs e)
         {
             Resources.DisposeAll();
-            world.Dispose();
-            origin.Dispose();
-            player.Dispose();
-            hotbar.Dispose();
+            game.Dispose();
         }
     }
 }
