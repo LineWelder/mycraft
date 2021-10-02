7003a9499a788639fbbd5374e3f3eea77d9403c0
LineWelder <redalerm@gmail.com>
2021-10-02 10:11:00 +0200

    Added flat world generator

---
+++ World/Generation/FlatWorldGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Mycraft.Blocks;
+
+namespace Mycraft.World.Generation
+{
+    public class FlatWorldGenerator : IWorldGenerator
+    {
+        public void GenerateChunk(Chunk chunk)
+        {
+            for (int x = 0; x < Chunk.SIZE; x++)
+                for (int z = 0; z < Chunk.SIZE; z++)
+                {
+                    for (int y = 0; y < Chunk.HEIGHT; y++)
+                        if (y < 16)
+                            chunk.blocks[x, y, z] = BlockRegistry.Stone;
+                        else if (16 <= y && y < 19)
+                            chunk.blocks[x, y, z] = BlockRegistry.Dirt;
+                        else if (y == 19)
+                            chunk.blocks[x, y, z] = BlockRegistry.Grass;
+                        else
+                            chunk.blocks[x, y, z] = BlockRegistry.Air;
+
+                    chunk.groundLevel[x, z] = 19;
+                }
+        }
+    }
+}
