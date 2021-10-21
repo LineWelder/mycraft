2f89b9b909f882d6389eec4e075c9205683facac
LineWelder <redalerm@gmail.com>
2021-10-21 19:57:08 +0200

    Added LightMap class for a monochromatic 3d texture

---
+++ Graphics/LightMap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using OpenGL;
+
+namespace Mycraft.Graphics
+{
+    public class LightMap : IDisposable
+    {
+        public readonly uint glId;
+
+        public unsafe float[,,] Data
+        {
+            set
+            {
+                fixed (float* valuePtr = &value[0, 0, 0])
+                {
+                    Gl.TexImage3D(
+                        TextureTarget.Texture3d, 0,
+                        InternalFormat.Luminance8,
+                        value.GetLength(0), value.GetLength(1), value.GetLength(2), 0,
+                        OpenGL.PixelFormat.Luminance, PixelType.Float,
+                        new IntPtr(valuePtr)
+                    );
+                }
+            }
+        }
+
+        public LightMap()
+        {
+            glId = Gl.GenTexture();
+            Gl.BindTexture(TextureTarget.Texture3d, glId);
+            
+            Gl.TexParameteri(TextureTarget.Texture3d, TextureParameterName.TextureWrapS, TextureWrapMode.ClampToEdge);
+            Gl.TexParameteri(TextureTarget.Texture3d, TextureParameterName.TextureWrapT, TextureWrapMode.ClampToEdge);
+            Gl.TexParameteri(TextureTarget.Texture3d, TextureParameterName.TextureWrapR, TextureWrapMode.ClampToEdge);
+            Gl.TexParameteri(TextureTarget.Texture3d, TextureParameterName.TextureMinFilter, TextureMinFilter.Linear);
+            Gl.TexParameteri(TextureTarget.Texture3d, TextureParameterName.TextureMagFilter, TextureMagFilter.Linear);
+        }
+
+        public void Bind() => Gl.BindTexture(TextureTarget.Texture3d, glId);
+        public void Dispose() => Gl.DeleteTextures(glId);
+    }
+}
