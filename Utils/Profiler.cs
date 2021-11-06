b449796a066b127cb79fb5d0f7b0f160bb0bf913
LineWelder <redalerm@gmail.com>
2021-11-07 02:19:00 +0300

    Created a simple profiler for easier profiling

---
+++ Utils/Profiler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Mycraft.Utils
+{
+    public class Profiler
+    {
+        private readonly Stopwatch frameStopwatch;
+        private readonly Stopwatch fragmentStopwatch;
+        private readonly Dictionary<string, long> fragments;
+
+        public Profiler()
+        {
+            frameStopwatch = new Stopwatch();
+            fragmentStopwatch = new Stopwatch();
+            fragments = new Dictionary<string, long>();
+        }
+
+        public void NewFrame()
+        {
+            fragments.Clear();
+            frameStopwatch.Restart();
+            fragmentStopwatch.Restart();
+        }
+
+        public void EndFragment(string name)
+        {
+            fragments.Add(name, fragmentStopwatch.ElapsedMilliseconds);
+            fragmentStopwatch.Restart();
+        }
+
+        public void EndFrame()
+        {
+            frameStopwatch.Stop();
+            fragmentStopwatch.Stop();
+        }
+
+        public void PrintInfo()
+        {
+            if (frameStopwatch.IsRunning)
+            {
+                EndFrame();
+            }
+
+            Console.WriteLine("Total frame time: " + frameStopwatch.ElapsedMilliseconds);
+            foreach (var pair in fragments)
+            {
+                Console.WriteLine($"{pair.Key} time: {pair.Value}");
+            }
+        }
+    }
+}
